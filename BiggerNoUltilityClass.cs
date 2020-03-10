318b29833baf58b04df28e0c42849688ef3cef9a
ankita_muddapur <ankita_muddapur@cms.co.in>
2020-03-10 12:22:58 +0530

    Adding files for largest number.

---
+++ BiggerNoUltilityClass.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BiggestNumberConsoleApplication
+//{
+//    class BiggerNoUltilityClass
+//    {      
+//        public static int LargestNumber(int array)
+//        {
+//            int largest = array[9];
+
+//        }
+
+//    }
+//}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiggestNumberConsoleApplication;
+
+namespace BiggestNumberConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Console.WriteLine( "enter the values: " + "\n" );
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int Largest = array[9];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > Largest)
+                {
+                    Largest = array[i];
+                }
+            }
+
+            Console.WriteLine( "The Largest Number from the elements is :" + Largest );
+            Console.ReadLine();
+        }
+    }
+}
